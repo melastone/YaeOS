75dc754df0938b87dde0456baa63a72581167f93
melastone <marcylu@hotmail.it>
2018-02-16 15:28:01 +0100

    Modificati i file const.h e pcb.c

--- phase1/pcb.c
+++ phase1/pcb.c
@@ -21,6 +21,7 @@ pcb_t *pcbfree_h  /* testa della lista pcbFree */
 
 /************************* Allocazione e deallocazione dei PCB ************************/
 
+
 /// Inizializza la pcbFree in modo da contenere tutti gli elementi della pcbFree_table. 
 /// Questo metodo deve essere chiamato una volta sola in fase di inizializzazione della 
 /// struttura dati.
@@ -47,6 +48,7 @@ pcb_t *allocPcb() {
 
 
 /****************************** Gestione delle code dei PCB ******************************/
+
 
 /// Inserisce l'elemento puntato da p nella coda dei processi puntata da head.
 /// L'inserimento deve avvenire tenendo conto della priorità di ciascun PCB.
@@ -87,14 +89,75 @@ void forallProcQ(pcb_t *head, void fun(pcb_t *pcb, void *), void *arg) {
 
 /****************************** Gestione dell'albero dei PCB *****************************/
 
+
+///Inserisce il PCB puntato da p come fratello del PCB puntato da sib.
+void insertSibling(pcb_t *sib, pcb_t *p) {
+
+	if (sib->p_sib = NULL) {
+		
+		sib->p_sib = p ;
+		return ;
+
+	}
+
+	return insertSibling(sib->p_sib, p) ;
+}
+
+
+///Rimuove il PCB puntato da p se si trova nella posizione successiva del PCB puntato da sib.
+pcb_t *removeSibling(pcb_t *sib, pcb_t *p) {
+
+	if (sib->p_sib = p) {
+
+		pcb_t *tmp = p ;
+		sib->p_sib = p->p_sib ;
+		tmp->p_parent, tmp->p_sib = NULL ;
+		return tmp ;
+
+	}
+
+	//Se p non è il fratello successivo di sib 
+	return removeSibling(sib->p_sib, p) ;
+
+}
+
+
 /// Inserisce il PCB puntato da p come figlio del PCB puntato da parent.
 void insertChild(pcb_t *parent, pcb_t *p) {
 
+	p->p_parent = parent ;
+
+	//Se parent non  ha nessun figlio
+	if (parent->p_first_child = NULL) {
+
+    	parent->p_first_child = p ;
+    	return;
+	
+	}
+
+	//Se invece ci sono già altri figli
+	else {
+
+		return insertSibling(parent->p_first_child, p) ;
+
+	}
 }
 
 
 /// Rimuove il primo figlio del PCB puntato da p. Se p non ha figli restituisce NULL.
 pcb_t *removeChild(pcb_t *p) {
+
+	//Se p non ha figli
+	if (p->p_first_child = NULL) {
+
+		return NULL ;
+
+	}
+
+	pcb_t *child = p->p_first_child ;
+	p->p_first_child = child->p_sib ;
+	child->p_parent, child->p_sib = NULL ;
+	return child ;
 
 }
 
@@ -104,5 +167,20 @@ pcb_t *removeChild(pcb_t *p) {
 /// A differenza della removeChild, p può trovarsi in una posizione arbitraria (ossia non è
 /// necessariamente il primo figlio del padre).
 pcb_t *outChild(pcb_t *p) {
+
+	if (p->p_parent = NULL) {
+
+		return NULL;
+
+	}
+
+	//Se p è il primo figlio
+	if (p->p_parent->p_first_child = p) {
+
+		return removeChild(p->p_parent) ;
+	}
+
+	//Se invece p si trova in un'altra posizione
+	return removeSibling(p->p_parent->p_first_child, p) ;
 
 }
