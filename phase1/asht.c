a212c37b955bf90ac6487b10c31f9cd3539a23df
Lorenzo Turrini <turrini97lorenzo@gmail.com>
2018-02-19 20:13:15 +0100

    Implementati i metodi: INSERTBLOCKED, HEADBLOCKED, FORALLBLOCKED  e INIASL(da controllare)

--- phase1/asht.c
+++ phase1/asht.c
@@ -16,50 +16,93 @@
 #include "../const.h"
 #include "../types.h"
 
-//puntatore allla testa della lista  dei SEMD  liberi
+//puntatore allla testa della lista dei SEMD  liberi
 semd_t *semdFree_h
-
 //hash table dei semafori attivi.
 semd_t *semdhash[ASHDSIZE]
+static semd_t semd_table[MAXSEMD];
+
 
 void initASL(){
-	
-	static semd_t semd_table[MAXSEMD];
 
-	int i=0;
-	initASLRic(i, semd_t semd_table[MAXSEMD], semdFree_h, NULL);
+	semdFree_h = NULL;
+	int i = 0;
+	initASLRic(i, &semd_table[i]);
 }
 
-void initASLRic(int i, semd_t semdArray[], semd_t * semdFree_head, semd_t * semdFree_succ){
-	if(i == 0){
-		semdFree_head = &semdArray[i];
-		semdFree_head->s_next = semdFree_prec;
-		i++;
-	}
-	else if(i < MAXSEMD-1){
-		semdFree_succ = &semdArray[i];
-		semdFree_succ->s_next = NULL;
-		semdFree_head->s_next = semdFree_succ;
-		i++;
-	}
-	initASLRic(i,semdArray[],semdFree_head,semdFree_head->s_next);
-}
+
 
 int insertBlocked(int *key, pcb_t *p){
+	int indice = hash(key);
+	semd_t *trovato = getSemByKey(semdhash[indice],key);
+	if(trovato != NULL){
+		p->p_semKey = key; //assegno la chiave del SEMD anche nel PCB
+		insertProcQ(trovato->s_procQ, p); 
+		return 0;
+	}else{
+		if(semdFree_h != NULL){
+			semd_t * semdFree = semdFree_h; //prendo un SEMD dalla lista di quelli liberi
+			semdFree_h = semdFree_h->s_next; //tolgo SEMD che ho appena preso
+			//setto i campi del SEMD
+			semdFree->s_next = NULL;
+			semdFree->s_key = key;
+			//setto la key del processo
+			p->p_semKey = key;
+			semdFree->s_procQ = p;
+			//inserisco SEMD nel HASH TABLE
+			semdInsert(&semdhash[n],semdFree);
+			return 0;
+		}
+	}
+	return -1;
+}
 
-// Searching active sempahore whose identifier is key
+pcb_t *headBlocked(int *key){
+	int indice = hash(key);
+	semd_t *trovato = getSemByKey(semdhash[indice],key);
+	if(trovato != NULL){
+		return trovato->s_procQ;
+	}
+	
+	return NULL;
+}
 
-// The searched active semaphore doesn't exist (it may not be active)
-
-// Setting the blocked ProcBlk to the semaphore and adding it to the tail of
-// process queue of the semaphore
-
-
+void forallBlocked(int *key, void fun(pcb_t *pcb, void *), void *arg){
+	pcb_t *tmp = headBlocked(key);
+	if(tmp != NULL){
+		forallProcQ(tmp, fun, arg);
+	}
 
 }
 
+/************************* FUNZIONI AUSILIARIE ************************/
 
+//funzione ausiliaria per inizializzare la lista dei semafori liberi
+void initASLRic(int i, semd_t * semdElement){
+	
+	if (i<MAXSEMD) {
+		semdElement->s_next = semdFree_h;
+		semdFree_h = semdEle = key;ment;
+	}
+	initASLRic(i++, &semd_table[i]);
+}
 
-// Metodi ausiliari
+//ricerca per key del SEMD all'interno del HASH TABLE 
+semd_t * getSemByKey(semd_t *semdhash,int *key){
+	if(semdhash != NULL){
+		if(semdhash->s_key == key){
+			return semdhash;
+		}
+		return getSemByKey(semdhash->s_next,key);
+	}
+	return NULL;
+}
 
-semd_t* getSemByKey(int *key, semd_t* semdPointer)
+//Inserimento nuovo SEMD all'interno del HASH TABLE
+void semdInsert(semd_t **semdhash, semd_t *semdFree){
+	if(*semdhash == NULL){
+		*semdhash = semdFree;
+	}else{
+		semdInsert(&(*semdhash)->s_next,semdFree);
+	}
+}
