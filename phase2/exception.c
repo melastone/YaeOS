eca6cd84085108b657f8fcd8e31adbbddb52a58e
melastone <melania.ghelli@studio.unibo.it>
2018-09-17 17:10:30 +0200

    Implementata la SYS1. Creato il file header exception.h. Inseriti in const.h i valori di default per le syscall.

--- phase2/exception.c
+++ phase2/exception.c
@@ -11,9 +11,42 @@
 //  Lorenzo Turrini
 //  Lorenzo Poluzzi
 //  Melania Ghelli
-//
+//	
 
 #include <const.h>
+#include "./h/exception.h"
+#include "../h/pcb.h"
+
+/*********************************** Funzioni ausiliarie *********************************/
+
+// Realizza una copia di tutti i campi di una struttura state_t, a partire dall'attuale 
+// state nella nuova newState
+void copyState(state_t* state, state_t* newState) {
+    
+    newState->a1 = state->a1;
+    newState->a2 = state->a2;
+    newState->a3 = state->a3;
+    newState->a4 = state->a4;
+    newState->v1 = state->v1;
+    newState->v2 = state->v2;
+    newState->v3 = state->v3;
+    newState->v4 = state->v4;
+    newState->v5 = state->v5;
+    newState->v6 = state->v6;
+    newState->sl = state->sl;
+    newState->fp = state->fp;
+    newState->ip = state->ip;
+    newState->sp = state->sp;
+    newState->lr = state->lr;
+    newState->pc = state->pc;
+    newState->cpsr = state->cpsr;
+    newState->CP15_Control = state->CP15_Control;
+    newState->CP15_EntryHi = state->CP15_EntryHi;
+    newState->CP15_Cause = state->CP15_Cause;
+    newState->TOD_Hi = state->TOD_Hi;
+    newState->TOD_Low = state->TOD_Low; 
+
+}
 
 /*************************************** System Call *************************************/
 // SYSCALL(unsigned int sysNum, unsigned int arg1, unsigned int arg2, unsigned
@@ -27,7 +60,38 @@
 // Se la system call ha successo il valore di ritorno è zero altrimenti è -1. 
 // Se la chiamata ha successo cpid contiene l’identificatore del processo figlio
 // (indirizzo del PCB).
-int SYSCALL (CREATEPROCESS, state t *statep, int priority, void **cpid)
+// Al momento della chiamata di questa operazione, il processo chiamante salva nei propri 
+// registri (campi di statep, istanza della struttura dati state_t definita in 
+// /usr/include/uarm/uARMtypes.h) le seguenti informazioni:
+// -a1 contiene la costante 1; nello stesso registro verranno poi memorizzate le informazioni
+//  di esito della chiamata di SYS1
+// -a2 contiene l'indirizzo fisico di uno state_t da usare come stato iniziale del nuovo processo
+// -a3 contiene il livello di priorità 
+int SYSCALL (CREATEPROCESS, state_t *statep, int priority, void **cpid) {
+
+	pcb_t newP = allocPcb() ;
+
+	// Se non ci sono pcb liberi la funzione deve ritornare -1
+	if (newP != NULL) {
+
+		(newP->p_priority) = priority ;
+
+		processCount++ ;
+
+		// Setto i valori di state_t del nuovo processo come quelli del genitore
+		copyState(statep, &(newP->p_s)) ;
+
+		// Creo i legami di parentela e inserisco il nuovo processo nella coda dei ready
+		insertChild(curProc, newP) ;
+		insertProcQ(&readyQueue, newP) ;
+
+		cpid = &(newP) ;
+		return 0 ;
+
+	}
+
+	return -1 ;
+}
 
 /// SYS2
 // Termina il processo indicato (o il processo chiamante se pid==NULL) e tutta la progenie 
@@ -46,7 +110,6 @@ void SYSCALL (SEMP, int *semaddr)
 // Realizza l'operazione di V su un semaforo. Il valore del semaforo è memorizzato nella
 // variabile di tipo intero passata per indirizzo. L’indirizzo della variabile agisce da
 // indentificatore del semaforo.
-
 void SYSCALL (SEMV, int *semaddr)
 
 /// SYS5
@@ -56,7 +119,7 @@ void SYSCALL (SEMV, int *semaddr)
 // codice della ROM: quando avviene una trap da passare al gestore lo stato del processo 
 // che ha causato la trap viene posto nell’area old e viene caricato lo stato presente 
 // nell’area new. La system call deve essere richiamata una sola volta per tipo.
-/ S/e la system call ha successo restituisce 0 altrimenti -1.
+// Se la system call ha successo restituisce 0 altrimenti -1.
 int SYSCALL (SPECHDL, int type, state_t *old, state_t *new)
 
 /// SYS6
